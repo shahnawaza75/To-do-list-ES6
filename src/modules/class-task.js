2a812ed04aa09173abb39a200d9f24d9ea100303
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-05-13 11:58:12 +0530

    Edit JavaScript file

---
+++ src/modules/class-task.js
@@ -0,0 +1,8 @@
+export default class Task {
+    constructor(_description, _id, _completed = false) {
+      this.description = _description;
+      this.id = _id;
+      this.completed = _completed;
+    }
+  }
+  
