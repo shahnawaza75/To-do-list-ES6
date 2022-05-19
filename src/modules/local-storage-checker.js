f391711ed803750005d8361063c3a37979022870
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-05-19 21:39:14 +0530

    Added files

--- src/modules/local-storage-checker.js
+++ src/modules/local-storage-checker.js
@@ -9,12 +9,12 @@ const isStorageAvailable = (type) => {
     return true;
   } catch (e) {
     return e instanceof DOMException && (
-    // 1) everything except Firefox
-    // 2) Firefox
-    // test name field too, because code might not be present
-    // 3) everything except Firefox
-    // 4) Firefox
-    // 5) acknowledge QuotaExceededError only if there's something already stored
+      // 1) everything except Firefox
+      // 2) Firefox
+      // test name field too, because code might not be present
+      // 3) everything except Firefox
+      // 4) Firefox
+      // 5) acknowledge QuotaExceededError only if there's something already stored
       e.code === 22 || e.code === 1014 || e.name === 'QuotaExceededError' || e.name === 'NS_ERROR_DOM_QUOTA_REACHED') && (storage && storage.length !== 0);
   }
 };
