1b048cf614b36d30206d26dfa87a10d6e2ed4fe7
Stanmegga <ezeokestanley.ifeanyi@gmail.com>
2022-05-20 17:42:33 +0100

    Add create .test JS functionality

--- tests/class-task-list.test.js
+++ tests/class-task-list.test.js
@@ -6,6 +6,24 @@ import TaskList from '../modules/class-task-list.js';
 
 // Testing Add Item Todo
 
+describe('Testing Add Item', () => {
+  test('Adding todo to array not to be null', () => {
+    const todo = new TaskList();
+    todo.addTask('WakeUp', false, 0);
+
+    expect(todo.length).not.toBeNull();
+  });
+  it('Adding new task', () => {
+    const todo = new TaskList();
+    todo.addTask('Practice', false, 1);
+
+    expect(todo.data[1].description).toEqual('Practice');
+  });
+  it('Adding todo to return details property', () => {
+    const todo = new TaskList();
+    todo.addTask('Sleep', false, 2);
+
+   
 
 // Testing Remove Item Todo
 
