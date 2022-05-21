da65261049a999b80e47905bb47451b931c246c1
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-05-21 18:37:08 +0530

    added files

--- src/tests/class-task-list.test.js
+++ src/tests/class-task-list.test.js
@@ -1,22 +1,70 @@
-import Task from '../modules/class-task-list';
+/**
+ * @jest-environment jsdom
+ */
 
-const tasksObject = new Task();
-describe('Task function', () => {
-  tasksObject.add('task 0');
-  tasksObject.add('task 1');
-  tasksObject.add('task 2');
-  tasksObject.add('task 3');
-  tasksObject.add('task 4');
+ import TaskList from '../modules/class-task-list.js';
 
-  test('Add function', () => {
-    expect(tasksObject.arrTasks[0].description).toBe('task 0');
-    expect(tasksObject.arrTasks.length).toBe(5);
-    expect(tasksObject.length).not.toBe(0);
-  });
-
-  test('Remove function', () => {
-    tasksObject.remove(1);
-    expect(tasksObject.arrTasks[1].description).toBe('task 2');
-    expect(tasksObject.arrTasks.length).toBe(4);
-  });
-});
+ // Testing Add Item Todo
+ 
+ describe('Testing Add Item', () => {
+   test('Adding todo to array not to be null', () => {
+     const todo = new TaskList();
+     todo.addTask('WakeUp', false, 0);
+ 
+     expect(todo.length).not.toBeNull();
+   });
+   it('Adding new task', () => {
+     const todo = new TaskList();
+     todo.addTask('Practice', false, 1);
+ 
+     expect(todo.data[1].description).toEqual('Practice');
+   });
+   it('Adding todo to return details property', () => {
+     const todo = new TaskList();
+     todo.addTask('Sleep', false, 2);
+ 
+     expect(todo.data[2].description).toBe('Sleep');
+   });
+   it('Adding todo to return boolean', () => {
+     const todo = new TaskList();
+     todo.addTask('Cook', true, 3);
+   });
+   it('Adding todo to return index', () => {
+     const todo = new TaskList();
+     todo.addTask('Singing', false, 4);
+ 
+     expect(todo.data[4].id - 1).toEqual(4);
+   });
+ });
+ 
+ // Testing Remove Item Todo
+ 
+ describe('Testing Remove Item', () => {
+   it('Removing todo from array to be null', () => {
+     const todo = new TaskList();
+     todo.addTask('Drive', false, 5);
+     todo.removeTask(5);
+ 
+     expect(todo[5]).toBeUndefined();
+   });
+   it('Removing todo from a multiple added todos which returns details', () => {
+     const todo = new TaskList();
+     todo.addTask('Read', false, 4);
+     todo.removeTask(5);
+ 
+     expect(todo.data[4].description).toEqual('Read');
+   });
+   it('Removing  todo from a multiple added todos which return length', () => {
+     const todo = new TaskList();
+     todo.addTask('Pray', false, 5);
+     todo.addTask('Lunch', false, 6);
+     todo.removeTask(3);
+ 
+     expect(todo.data).toHaveLength(6);
+   });
+   it('remove', () => {
+     const todo = new TaskList();
+     todo.removeTask(4);
+     expect(todo.data[4].description).toEqual('Lunch');
+   });
+ });
