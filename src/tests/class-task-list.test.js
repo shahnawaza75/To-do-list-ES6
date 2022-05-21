906acce30849f4fe4d7a8656f4b9b1dfe356b7e7
Stanmegga <ezeokestanley.ifeanyi@gmail.com>
2022-05-21 17:41:40 +0100

    Add testing of remove items

--- src/tests/class-task-list.test.js
+++ src/tests/class-task-list.test.js
@@ -37,3 +37,34 @@ describe('Testing Add Item', () => {
   });
 });
 
+// Testing Remove Item Todo
+
+describe('Testing Remove Item', () => {
+  it('Removing todo from array to be null', () => {
+    const todo = new TaskList();
+    todo.addTask('Drive', false, 5);
+    todo.removeTask(5);
+
+    expect(todo[5]).toBeUndefined();
+  });
+  it('Removing todo from a multiple added todos which returns details', () => {
+    const todo = new TaskList();
+    todo.addTask('Read', false, 4);
+    todo.removeTask(5);
+
+    expect(todo.data[4].description).toEqual('Read');
+  });
+  it('Removing  todo from a multiple added todos which return length', () => {
+    const todo = new TaskList();
+    todo.addTask('Pray', false, 5);
+    todo.addTask('Lunch', false, 6);
+    todo.removeTask(3);
+
+    expect(todo.data).toHaveLength(6);
+  });
+  it('remove', () => {
+    const todo = new TaskList();
+    todo.removeTask(4);
+    expect(todo.data[4].description).toEqual('Lunch');
+  });
+});
