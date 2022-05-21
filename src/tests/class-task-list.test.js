7fb7209a19daee89be6a93e07b489e0c56fd2ece
Stanmegga <ezeokestanley.ifeanyi@gmail.com>
2022-05-21 17:40:28 +0100

    Add .test testing functionality to class task list

--- src/tests/class-task-list.test.js
+++ src/tests/class-task-list.test.js
@@ -11,4 +11,29 @@ describe('Testing Add Item', () => {
     const todo = new TaskList();
     todo.addTask('WakeUp', false, 0);
 
-   
+    expect(todo.length).not.toBeNull();
+  });
+  it('Adding new task', () => {
+    const todo = new TaskList();
+    todo.addTask('Practice', false, 1);
+
+    expect(todo.data[1].description).toEqual('Practice');
+  });
+  it('Adding todo to return details property', () => {
+    const todo = new TaskList();
+    todo.addTask('Sleep', false, 2);
+
+    expect(todo.data[2].description).toBe('Sleep');
+  });
+  it('Adding todo to return boolean', () => {
+    const todo = new TaskList();
+    todo.addTask('Cook', true, 3);
+  });
+  it('Adding todo to return index', () => {
+    const todo = new TaskList();
+    todo.addTask('Singing', false, 4);
+
+    expect(todo.data[4].id - 1).toEqual(4);
+  });
+});
+
