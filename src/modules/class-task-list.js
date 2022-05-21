3b51652844eea4e21e1595fb24fb982c74d617c5
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-05-21 16:39:59 +0530

    fixed the test file errors.

--- src/modules/class-task-list.js
+++ src/modules/class-task-list.js
@@ -1,82 +1,34 @@
-import Task from './class-task.js';
-import isStorageAvailable from './local-storage-checker.js';
-
-const storageName = 'taskList';
-
-export default class TaskList {
-  constructor() {
-    this.data = [];
-    this.id = 1;
-    this.initUpdate();
-  }
-
-  initUpdate() {
-    if (isStorageAvailable('localStorage')) {
-      const data = JSON.parse(localStorage.getItem(storageName));
-
-      if (data && data.length !== 0) {
-        this.data = JSON.parse(localStorage.getItem(storageName));
-
-        const lastItem = this.data.reduce((prev, current) => {
-          const val = prev.id > current.id ? prev : current;
-          return val;
-        });
-        this.id = lastItem.id + 1;
+export default class Task {
+    constructor(arrTasks) {
+      this.arrTasks = arrTasks || [];
+    }
+  
+    add = (description) => {
+      const completed = false;
+      const index = this.arrTasks.length;
+      this.arrTasks.push({
+        description,
+        completed,
+        index,
+      });
+    }
+  
+    remove = (index) => {
+      this.arrTasks = this.arrTasks.filter((task) => (task.index !== index));
+      for (let i = index; i < this.arrTasks.length; i += 1) {
+        this.arrTasks[i].index -= 1;
       }
     }
-  }
-
-  addTask(task) {
-    if (isStorageAvailable) {
-      const taskObj = new Task(task, this.id);
-
-      this.data.push(taskObj);
-      localStorage.setItem(storageName, JSON.stringify(this.data));
-
-      this.id += 1;
+  
+    edit = (index, description) => {
+      this.arrTasks[index].description = description;
+    }
+  
+    changeCompletedStatus = (index) => {
+      this.arrTasks[index].completed = !this.arrTasks[index].completed;
+    }
+  
+    removeCompletedTask = () => {
+      this.arrTasks = this.arrTasks.filter((task) => (task.completed === false));
     }
   }
-
-  removeTask(taskID) {
-    this.data = this.data.filter((i) => i.id !== taskID);
-    this.resetIds();
-    localStorage.setItem(storageName, JSON.stringify(this.data));
-  }
-
-  updateStatus(taskID, status) {
-    this.data = this.data.map((obj) => {
-      if (obj.id === taskID) {
-        return { ...obj, completed: status };
-      }
-
-      return obj;
-    });
-    localStorage.setItem(storageName, JSON.stringify(this.data));
-  }
-
-  renameTask(taskID, newTask) {
-    this.data = this.data.map((obj) => {
-      if (obj.id === taskID) {
-        return { ...obj, description: newTask };
-      }
-
-      return obj;
-    });
-    localStorage.setItem(storageName, JSON.stringify(this.data));
-  }
-
-  clearCompleted() {
-    this.data = this.data.filter((i) => i.completed === false);
-    this.resetIds();
-    localStorage.setItem(storageName, JSON.stringify(this.data));
-  }
-
-  resetIds() {
-    let i = 1;
-    this.data.forEach((e) => {
-      e.id = i;
-      i += 1;
-    });
-    this.id = i;
-  }
-}
